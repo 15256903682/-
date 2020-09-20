47817b6184feddd71a081be13d7259a757cb2b79
krr <1063761410@qq.com>
2020-09-20 20:56:10 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -32,10 +32,9 @@ router.beforeEach((to, from, next) => {
     document.title = `${to.meta.title} | vue-manage-system`;
     const role = localStorage.getItem('ms_username');
     // const role = true
-    // if (!role && to.path !== '/login') {
-    //     next('/login');
-    // } else 
-    if (to.meta.permission) {
+    if (!role && to.path !== '/login') {
+        next('/login');
+    } else if (to.meta.permission) {
         // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
         role === 'admin' ? next() : next('/403');
     } else {
